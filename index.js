0d0eec1d1aebbc5d6a79f6cfe3a4624a79847e3c
Mihika Saraf <mihikaps003@gmail.com>
2024-09-19 23:00:33 +0530

    Check before merging

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log("beech beech mein add karo - mihika")
 console.log("I hate pedos - mihika'24")
 console.log("This is kedar")
 console.log("THis is kedar 2")
+
+console.log("Request to admin for merge - mihika")
